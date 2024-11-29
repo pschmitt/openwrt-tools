9b29252523f49bfb8c6776b9eed7775f883ffb0f
Philipp Schmitt <philipp@schmitt.co>
2024-11-29 14:47:11 +0100

    time for an echo lib

---
+++ lib/echo.sh
@@ -0,0 +1,81 @@
+# vim: set ft=sh:
+# shellcheck shell=bash
+
+echo_fancy() {
+  local prefix="$1"
+  local color="$2"
+  shift 2
+
+  if [[ -n "$NO_COLOR" ]]
+  then
+    echo "$prefix $*" >&2
+    return 0
+  fi
+
+  echo -e "\e[1m\e[34m${prefix}\e[0m $*" >&2
+}
+
+echo_info() {
+  local prefix="INF"
+  local color='\e[1m\e[34m'
+
+  echo_fancy "$prefix" "$color" "$*"
+}
+
+# shellcheck disable=SC2317
+echo_success() {
+  local prefix="OK"
+  local color='\e[1m\e[32m'
+
+  echo_fancy "$prefix" "$color" "$*"
+}
+
+# shellcheck disable=SC2317
+echo_warning() {
+  [[ -n "$NO_WARNING" ]] && return 0
+  local prefix="WRN"
+  local color='\e[1m\e[33m'
+
+  echo_fancy "$prefix" "$color" "$*"
+}
+
+# shellcheck disable=SC2317
+echo_error() {
+  local prefix="ERR"
+  local color='\e[1m\e[31m'
+
+  echo_fancy "$prefix" "$color" "$*"
+}
+
+# shellcheck disable=SC2317
+echo_debug() {
+  [[ -z "${DEBUG}${VERBOSE}" ]] && return 0
+  local prefix="DBG"
+  local color='\e[1m\e[35m'
+
+  echo_fancy "$prefix" "$color" "$*"
+}
+
+# shellcheck disable=SC2317
+echo_dryrun() {
+  local prefix="DRY"
+  local color='\e[1m\e[35m'
+
+  echo_fancy "$prefix" "$color" "$*"
+}
+
+echo_confirm() {
+  if [[ -n "$NOCONFIRM" ]]
+  then
+    return 0
+  fi
+
+  local msg_pre=$'\e[31mASK\e[0m'
+  local msg="${1:-"Continue?"}"
+  local yn
+  read -r -n1 -p "${msg_pre} ${msg} [y/N] " yn
+  [[ "$yn" =~ ^[yY] ]]
+  local rc="$?"
+  echo # append a NL
+  return "$rc"
+}
